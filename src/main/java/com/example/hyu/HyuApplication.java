f1681700d3dc7108a01dd13fb9eb7b39ca756e66
dongurig112 <donguring112@gmail.com>
2025-08-28 11:12:55 +0900

    init:spring boot project setup

---
+++ src/main/java/com/example/hyu/HyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hyu/HyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
